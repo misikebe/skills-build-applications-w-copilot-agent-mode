614542af9791df31f35063f2351668a3b2d1b139
misikebe <michal.becker@elitmind.com>
2026-05-19 16:19:59 +0000

    Testing 2

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        ironman = User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel)
+        captain = User.objects.create(name='Captain America', email='cap@marvel.com', team=marvel)
+        batman = User.objects.create(name='Batman', email='batman@dc.com', team=dc)
+        superman = User.objects.create(name='Superman', email='superman@dc.com', team=dc)
+
+        # Create activities
+        Activity.objects.create(user=ironman, type='Run', duration=30)
+        Activity.objects.create(user=batman, type='Swim', duration=45)
+        Activity.objects.create(user=superman, type='Bike', duration=60)
+        Activity.objects.create(user=captain, type='Yoga', duration=20)
+
+        # Create workouts
+        Workout.objects.create(name='Hero HIIT', description='High intensity for heroes')
+        Workout.objects.create(name='Power Yoga', description='Yoga for super strength')
+
+        # Create leaderboard
+        Leaderboard.objects.create(user=ironman, score=100)
+        Leaderboard.objects.create(user=batman, score=90)
+        Leaderboard.objects.create(user=superman, score=95)
+        Leaderboard.objects.create(user=captain, score=85)
+
+        self.stdout.write(self.style.SUCCESS('Test data populated for users, teams, activities, leaderboard, and workouts.'))
