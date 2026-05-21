eb1cb01d86a9001c2ecfd48ad8e985174aac0931
misikebe <michal.becker@elitmind.com>
2026-05-21 12:39:32 +0000

    Mongo

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -93,10 +93,7 @@ DATABASES = {
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
             'host': 'mongodb://localhost:27017',
-            'username': '',
-            'password': '',
-            'authSource': 'admin',
-            'authMechanism': 'SCRAM-SHA-1',
+            # No username or password for local dev
         },
     }
 }
